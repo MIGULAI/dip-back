a281f8045a418c59b5d367ab29ec5b66937819f5
Migulai <vitukivan05@gmail.com>
2023-09-03 19:27:59 +0300

    03-09-23

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,12 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta name="theme-color" content="#000000" />
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link rel="manifest" href="/manifest.webmanifest" />
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <title>Diplom Project</title>
-    <script defer="defer" src="/static/js/main.f0ae97b3.js"></script>
+    <script defer="defer" src="/static/js/main.fc791b00.js"></script>
     <link href="/static/css/main.aaa06fe2.css" rel="stylesheet">
 </head>
 
