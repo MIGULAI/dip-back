d0b009bc731e5b32d575b1d9e30a890548fac39f
Migulai <vitukivan05@gmail.com>
2023-11-07 22:19:47 +0200

    07-11-23 added one report

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <title>Diplom Project</title>
-    <script defer="defer" src="/static/js/main.6991acbe.js"></script>
+    <script defer="defer" src="/static/js/main.6c5d51e2.js"></script>
     <link href="/static/css/main.51b5d021.css" rel="stylesheet">
 </head>
 
