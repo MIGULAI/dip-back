1bbc0df0a21aebd05c47beba20bba35e1f3e1faf
Migulai <vitukivan05@gmail.com>
2023-11-26 18:00:36 +0200

    fixed modale tables

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,8 @@
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <title>Diplom Project</title>
-    <script defer="defer" src="/static/js/main.6c5d51e2.js"></script>
-    <link href="/static/css/main.51b5d021.css" rel="stylesheet">
+    <script defer="defer" src="/static/js/main.c8ff8797.js"></script>
+    <link href="/static/css/main.d085e99c.css" rel="stylesheet">
 </head>
 
 <body>
